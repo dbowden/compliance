7bf60344c4ef8452d36e4817a889fffc090efcc9
David <david@david-ubuntu.(none)>
2013-02-26 00:40:40 -0600

    still having trouble with levels

--- data_creation.R
+++ data_creation.R
@@ -4,7 +4,7 @@
 tc <- read.csv("http://dl.dropbox.com/u/4115584/tc2008.csv")
 icow <- read.csv("http://dl.dropbox.com/u/4115584/ICOW-Diehl.csv")
 frame <- read.csv("http://dl.dropbox.com/u/4115584/frame.csv")
-courts <- read.delim("C:/Users/David/Downloads/huthallee")
+courts <- read.delim("http://dl.dropbox.com/u/4115584/huthallee")
 
 #standardize NA codes
 tc[tc == "."] <- NA
@@ -51,7 +51,6 @@ icow$endclaim <- trncint(icow$endclaim, 6, 4)
 #colnames(icow)[6] <- "year" #this would capture new disputes
 colnames(icow)[7] <- "year" #this captures ongoing claims
 #icow$year[is.na(icow$year)] <- 2030
-rm(trncint)
 
 ############ Merge data #############
 
@@ -64,8 +63,19 @@ keeps <- c("gainer","loser","year","arbadj2")
 courts <- courts[names(courts) %in% keeps]
 rm(keeps)
 
+
+#merge
 tc$year.2 <- trncint(tc$year, 2, 2)
 tc <- merge(tc, courts, all.x=T, all.y=F)
+rm(courts,trncint)
+
+#make the NAs zeroes
+tc$arbadj2[is.na(tc$arbadj2)] <- 0
+
+#code more recent cases
+tc[367,24] <- 1
+tc[368,24] <- 1
+tc[370,24] <- 1
 
 #merge tc into frame
 data <- merge(tc, frame, all=T)
@@ -77,35 +87,40 @@ rm(icow)
 
 ################ Create 5, 10, and 20 year windows for wars ################
 
+library(plm)
+
+data$dyad <- paste(data$gainer,data$loser,sep="")
+lags <- pdata.frame(data, index=c("dyad","year"))
+
 #lead years of terr mids
-#data$mid0 <- ifelse(data$ter == 1, data$year, 0)
-data$mid1 <- ifelse(data$ter == 1, lag(data$year, 1), 0) 
-data$mid2 <- ifelse(data$ter == 1, lag(data$year, 2), 0)
-data$mid3 <- ifelse(data$ter == 1, lag(data$year, 3), 0)
-data$mid4 <- ifelse(data$ter == 1, lag(data$year, 4), 0)
-data$mid5 <- ifelse(data$ter == 1, lag(data$year, 5), 0)
-data$mid6 <- ifelse(data$ter == 1, lag(data$year, 6), 0)
-data$mid7 <- ifelse(data$ter == 1, lag(data$year, 7), 0)
-data$mid8 <- ifelse(data$ter == 1, lag(data$year, 8), 0)
-data$mid9 <- ifelse(data$ter == 1, lag(data$year, 9), 0)
-data$mid10 <- ifelse(data$ter == 1, lag(data$year, 10), 0)
-data$mid11 <- ifelse(data$ter == 1, lag(data$year, 11), 0)
-data$mid12 <- ifelse(data$ter ==1, lag(data$year, 12), 0)
-data$mid13 <- ifelse(data$ter ==1, lag(data$year, 13), 0)
-data$mid14 <- ifelse(data$ter ==1, lag(data$year, 14), 0)
-data$mid15 <- ifelse(data$ter ==1, lag(data$year, 15), 0)
-data$mid16 <- ifelse(data$ter ==1, lag(data$year, 16), 0)
-data$mid17 <- ifelse(data$ter ==1, lag(data$year, 17), 0)
-data$mid18 <- ifelse(data$ter ==1, lag(data$year, 18), 0)
-data$mid19 <- ifelse(data$ter ==1, lag(data$year, 19), 0)
-data$mid20 <- ifelse(data$ter ==1, lag(data$year, 20), 0)
+lags$mid0 <- ifelse(lags$ter == 1, lags$year, 0)
+lags$mid1 <- ifelse(lags$ter == 1, lag(lags$year, 1), 0) 
+lags$mid2 <- ifelse(lags$ter == 1, lag(lags$year, 2), 0)
+lags$mid3 <- ifelse(lags$ter == 1, lag(lags$year, 3), 0)
+lags$mid4 <- ifelse(lags$ter == 1, lag(lags$year, 4), 0)
+lags$mid5 <- ifelse(lags$ter == 1, lag(lags$year, 5), 0)
+lags$mid6 <- ifelse(lags$ter == 1, lag(lags$year, 6), 0)
+lags$mid7 <- ifelse(lags$ter == 1, lag(lags$year, 7), 0)
+lags$mid8 <- ifelse(lags$ter == 1, lag(lags$year, 8), 0)
+lags$mid9 <- ifelse(lags$ter == 1, lag(lags$year, 9), 0)
+lags$mid10 <- ifelse(lags$ter == 1, lag(lags$year, 10), 0)
+lags$mid11 <- ifelse(lags$ter == 1, lag(lags$year, 11), 0)
+lags$mid12 <- ifelse(lags$ter ==1, lag(lags$year, 12), 0)
+lags$mid13 <- ifelse(lags$ter ==1, lag(lags$year, 13), 0)
+lags$mid14 <- ifelse(lags$ter ==1, lag(lags$year, 14), 0)
+lags$mid15 <- ifelse(lags$ter ==1, lag(lags$year, 15), 0)
+lags$mid16 <- ifelse(lags$ter ==1, lag(lags$year, 16), 0)
+lags$mid17 <- ifelse(lags$ter ==1, lag(lags$year, 17), 0)
+lags$mid18 <- ifelse(lags$ter ==1, lag(lags$year, 18), 0)
+lags$mid19 <- ifelse(lags$ter ==1, lag(lags$year, 19), 0)
+lags$mid20 <- ifelse(lags$ter ==1, lag(lags$year, 20), 0)
 
 #if year is equal to leads, a mid occurred within the window
-data$w5 <- ifelse(is.na(data$year) == T, 0, ifelse(data$year == data$mid1 | data$year == data$mid2 | data$year == data$mid3 | data$year == data$mid4 | data$year == data$mid5, 1, 0))
+lags$w5 <- ifelse(is.na(lags$year) == T, 0, ifelse(lags$mid0 == 1 | lags$year == lags$mid1 | lags$year == lags$mid2 | lags$year == lags$mid3 | lags$year == lags$mid4 | lags$year == lags$mid5, 1, 0))
 
-data$w10 <- ifelse(is.na(data$year) == T, 0, ifelse(data$year == data$mid1 | data$year == data$mid2 | data$year == data$mid3 | data$year == data$mid4 | data$year == data$mid5 | data$year == data$mid6 | data$year == data$mid7 | data$year == data$mid8 | data$year == data$mid9 | data$year == data$mid10, 1, 0))
+lags$w10 <- ifelse(is.na(lags$year) == T, 0, ifelse(lags$year == lags$mid1 | lags$year == lags$mid2 | lags$year == lags$mid3 | lags$year == lags$mid4 | lags$year == lags$mid5 | lags$year == lags$mid6 | lags$year == lags$mid7 | lags$year == lags$mid8 | lags$year == lags$mid9 | lags$year == lags$mid10, 1, 0))
 
-data$w20 <- ifelse(is.na(data$year) == T, 0, ifelse(data$year == data$mid1 | data$year == data$mid2 | data$year == data$mid3 | data$year == data$mid4 | data$year == data$mid5 | data$year == data$mid6 | data$year == data$mid7 | data$year == data$mid8 | data$year == data$mid9 | data$year == data$mid10 | data$year == data$mid11 | data$year == data$mid12 | data$year == data$mid13 | data$year == data$mid14 | data$year == data$mid15 | data$year == data$mid16 | data$year == data$mid17 | data$year == data$mid18 | data$year == data$mid19 | data$year == data$mid20, 1, 0))
+lags$w20 <- ifelse(is.na(lags$year) == T, 0, ifelse(lags$year == lags$mid1 | lags$year == lags$mid2 | lags$year == lags$mid3 | lags$year == lags$mid4 | lags$year == lags$mid5 | lags$year == lags$mid6 | lags$year == lags$mid7 | lags$year == lags$mid8 | lags$year == lags$mid9 | lags$year == lags$mid10 | lags$year == lags$mid11 | lags$year == lags$mid12 | lags$year == lags$mid13 | lags$year == lags$mid14 | lags$year == lags$mid15 | lags$year == lags$mid16 | lags$year == lags$mid17 | lags$year == lags$mid18 | lags$year == lags$mid19 | lags$year == lags$mid20, 1, 0))
 
 # clean up
 drops <- c("mid0","mid1","mid2","mid3","mid4","mid5","mid6","mid7","mid8","mid9","mid10","mid11","mid12","mid13","mid14","mid15","mid16","mid17","mid18","mid19","mid20","V21","dyad")
