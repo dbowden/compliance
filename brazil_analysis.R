b21a93216b1899b1a7310fe1aa1a440a8f84765c
David Bowden <dbowden88@gmail.com>
2013-03-05 14:33:31 -0600

    almost done

--- brazil_analysis.R
+++ brazil_analysis.R
@@ -6,8 +6,45 @@ library(Zelig)
 library(stargazer)
 
 data$caprat = (data$cap_1/data$cap_2)
+data$lead1 = ifelse(data$lead1 > 0, 1, 0)
+data$lead1[is.na(data$lead)] = 0
+data$lagtc1 = ifelse(data$lagtc1 > 0, 1, 0)
+data$lagtc1[is.na(data$lagtc1)] = 0
 
-##################
 
-m1 = zelig(window5 ~ arbadj2 + agreement + conflict + contlose, data=data, model="logit")
+### Table 1: MIDs 1,5,10,20 years after trasnfer
+m1 = zelig(lead1 ~ agreement + conflict + contlose + caprat, data=data, model="logit")
 summary(m1)
+m2 = zelig(lead1 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, model="logit")
+summary(m2)
+m3 = zelig(window5 ~ agreement + conflict + contlose + caprat, data=data, model="logit")
+summary(m3)
+m4 = zelig(window5 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, model="logit")
+summary(m4)
+m5 = zelig(window10 ~ agreement + conflict + contlose + caprat, data=data, model="logit")
+summary(m5)
+m6 = zelig(window10 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, model="logit")
+summary(m6)
+m7 = zelig(window20 ~ agreement + contlose + conflict + caprat, data=data, model="logit")
+summary(m7)
+m8 = zelig(window20 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, model="logit")
+summary(m8)
+
+stargazer(m1,m2,m3,m4,m5,m6,m7,m8,title="Logit Models of the Effect of Legalized Territorial Transfers on Territorial MIDs",style="apsr")
+
+#### Claims
+
+m9 = zelig(lagtc1 ~ agreement + conflict + contlose + caprat, data=data, model="logit")
+summary(m9)
+m10 = zelig(lagtc1 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, model="logit")
+summary(m10)
+m11 = zelig(outstanding.claim5 ~ agreement + conflict + contlose + caprat, data=data, model="logit")
+summary(m11)
+m12 = zelig(outstanding.claim5 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, model="logit")
+summary(m12)
+
+### Predicted Probabilities
+
+mid1.no.agreement = setx(m2, agreement=0)
+mid1.agreement = setx(m2, agreement = 1)
+mid1.prob = sim(m2, x=mid1.no.agreement, x1=mid1.agreement)
