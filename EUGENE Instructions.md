72642f40b6ac5c59b0c772edb43785a110ae5060
David Bowden <dbowden88@gmail.com>
2013-09-03 17:08:31 -0500

    Added Eugene documentation

---
+++ EUGENE Instructions.md
@@ -0,0 +1,13 @@
+#Steps to Create Base Data in EUGENE#
+
+- Directed dyad years
+- All years, all dyads
+- No sampling
+- CINC score, maj power status, direct & colonial contiguity
+- COW dyadic MIDs w/ fatality/hostility levels, outcome, settlement, initiator, revision type
+- Side A as initiator, most serious if multiple MIDs in a year, only originators
+- Polity 2
+- Exclude based on Maoz
+- Include ongoing years if new dispute
+- Keep target vs initiator
+- Drop all joiners
--- brazil_analysis.R
+++ brazil_analysis.R
@@ -1,6 +1,6 @@
 #### Analysis for Brazil paper
 
-data <- read.csv("C:/Users/David/Desktop/Dropbox/RA Work/Compliance Paper/data/compliance/compliance.csv")
+data <- read.csv("compliance.csv")
 
 library(Zelig)
 library(stargazer)
@@ -33,6 +33,56 @@ m8 = glm(window20 ~ agreement + arbadj2 + contlose + conflict + caprat, data=dat
 summary(m8)
 
 stargazer(m1,m2,m3,m4,m5,m6,m7,m8,title="Logit Models of the Effect of Legalized Territorial Transfers on Territorial MIDs",style="apsr")
+
+########### Look at post-WWII
+
+h1 = glm(lead1 ~ agreement + conflict + contlose, data=data, family=binomial(logit), subset=year > 1945)
+summary(h1)
+h2 = glm(lead1 ~ agreement + arbadj2 + conflict + contlose , data=data, family=binomial(logit), subset=year > 1945)
+summary(h2)
+h3 = glm(window5 ~ agreement + conflict + contlose, data=data, family=binomial(logit), subset=year > 1945)
+summary(h3)
+h4 = glm(window5 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year > 1945)
+summary(h4)
+h5 = glm(window10 ~ agreement + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year > 1945)
+summary(h5)
+h6 = glm(window10 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year > 1945)
+summary(h6)
+h7 = glm(window20 ~ agreement + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year > 1945)
+summary(h7)
+h8 = glm(window20 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year > 1945)
+summary(h8)
+h9 = glm(lagtc1 ~ agreement + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year>1945)
+summary(h9)
+h10 = glm(lagtc1 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year>1945)
+summary(h10)
+
+stargazer(h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,title="Post-WWII",style="apsr")
+
+#### Pre-WWII
+
+j1 = glm(lead1 ~ agreement + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j1)
+j2 = glm(lead1 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j2)
+j3 = glm(window5 ~ agreement + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j3)
+j4 = glm(window5 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j4)
+j5 = glm(window10 ~ agreement + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j5)
+j6 = glm(window10 ~ agreement + arbadj2 + conflict + contlose + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j6)
+j7 = glm(window20 ~ agreement + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j7)
+j8 = glm(window20 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j8)
+j9 = glm(lagtc1 ~ agreement + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j9)
+j10 = glm(lagtc1 ~ agreement + arbadj2 + contlose + conflict + caprat, data=data, family=binomial(logit), subset=year < 1945)
+summary(j10)
+
+stargazer(j1,j2,j3,j4,j5,j6,j7,j8,j9,j10,title="Pre-WWII",style="apsr")
 
 # m1.5 = zelig(lead1 ~ agreement + conflict + contlose, data=data, model="relogit")
 # summary(m1.5)
@@ -76,7 +126,7 @@ i4 = zelig(window20 ~ agreement*conflict + contlose + caprat, data=data, family=
 summary(i4)
 i5 = zelig(lagtc1 ~ agreement*conflict + contlose + caprat, data=data, family=binomial(logit))
 summary(i5)
-i6 = zelig(outstanding.claim5 ~ agreement*conflict + contlose + caprat, data=data, family=binomial(logit))
+i6 = glm(outstanding.claim5 ~ agreement*conflict + contlose + caprat, data=data, family=binomial(logit))
 summary(i6)
 
 stargazer(i1,i2,i3,i4,i5,i6,title="hi",style="apsr")
